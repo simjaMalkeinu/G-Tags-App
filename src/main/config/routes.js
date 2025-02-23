8b9fa6f71b2b5d784a1097b180f6e4dc1cac83ae
Simja <yorkamacua@gmail.com>
2025-02-22 18:19:39 -0600

    ruta login

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -2,5 +2,6 @@ module.exports = {
   index: '../renderer/index.html',
   login: '../renderer/login.html',
   form1: '../renderer/form1.html',
-  form2: '../renderer/form2.html'
+  form2: '../renderer/form2.html',
+  app: '../renderer/app.html'
 }
